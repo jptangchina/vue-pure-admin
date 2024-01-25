aeea5bc65a69582707e4ed1af10dadbe657b5b68
jptang94 <jptang94@qq.com>
2024-01-25 14:26:30 +0800

    feat: 继续移除不需要的文件

--- mock/asyncRoutes.ts
+++ mock/asyncRoutes.ts
@@ -1,50 +1,12 @@
 // 模拟后端动态生成路由
 import { defineFakeRoute } from "vite-plugin-fake-server/client";
-import { system, permission, frame, tabs } from "@/router/enums";
+import { permission } from "@/router/enums";
 
 /**
  * roles：页面级别权限，这里模拟二种 "admin"、"common"
  * admin：管理员角色
  * common：普通角色
  */
-
-const systemRouter = {
-  path: "/system",
-  meta: {
-    icon: "setting",
-    title: "系统管理",
-    rank: system
-  },
-  children: [
-    {
-      path: "/system/user/index",
-      name: "SystemUser",
-      meta: {
-        icon: "flUser",
-        title: "用户管理",
-        roles: ["admin"]
-      }
-    },
-    {
-      path: "/system/role/index",
-      name: "SystemRole",
-      meta: {
-        icon: "role",
-        title: "角色管理",
-        roles: ["admin"]
-      }
-    },
-    {
-      path: "/system/dept/index",
-      name: "SystemDept",
-      meta: {
-        icon: "dept",
-        title: "部门管理",
-        roles: ["admin"]
-      }
-    }
-  ]
-};
 
 const permissionRouter = {
   path: "/permission",
@@ -74,151 +36,6 @@ const permissionRouter = {
   ]
 };
 
-const frameRouter = {
-  path: "/iframe",
-  meta: {
-    icon: "monitor",
-    title: "外部页面",
-    rank: frame
-  },
-  children: [
-    {
-      path: "/iframe/external",
-      meta: {
-        title: "文档外链"
-      },
-      children: [
-        {
-          path: "/external",
-          name: "https://yiming_chang.gitee.io/pure-admin-doc",
-          meta: {
-            title: "vue-pure-admin",
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/pureutilsLink",
-          name: "https://pure-admin-utils.netlify.app/",
-          meta: {
-            title: "pure-admin-utils",
-            roles: ["admin", "common"]
-          }
-        }
-      ]
-    },
-    {
-      path: "/iframe/embedded",
-      meta: {
-        title: "文档内嵌"
-      },
-      children: [
-        {
-          path: "/iframe/ep",
-          name: "FrameEp",
-          meta: {
-            title: "element-plus",
-            frameSrc: "https://element-plus.org/zh-CN/",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/iframe/tailwindcss",
-          name: "FrameTailwindcss",
-          meta: {
-            title: "tailwindcss",
-            frameSrc: "https://tailwindcss.com/docs/installation",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/iframe/vue3",
-          name: "FrameVue",
-          meta: {
-            title: "vue3",
-            frameSrc: "https://cn.vuejs.org/",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/iframe/vite",
-          name: "FrameVite",
-          meta: {
-            title: "vite",
-            frameSrc: "https://cn.vitejs.dev/",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/iframe/pinia",
-          name: "FramePinia",
-          meta: {
-            title: "pinia",
-            frameSrc: "https://pinia.vuejs.org/zh/index.html",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/iframe/vue-router",
-          name: "FrameRouter",
-          meta: {
-            title: "vue-router",
-            frameSrc: "https://router.vuejs.org/zh/",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        }
-      ]
-    }
-  ]
-};
-
-const tabsRouter = {
-  path: "/tabs",
-  meta: {
-    icon: "tag",
-    title: "标签页操作",
-    rank: tabs
-  },
-  children: [
-    {
-      path: "/tabs/index",
-      name: "Tabs",
-      meta: {
-        title: "标签页操作",
-        roles: ["admin", "common"]
-      }
-    },
-    // query 传参模式
-    {
-      path: "/tabs/query-detail",
-      name: "TabQueryDetail",
-      meta: {
-        // 不在menu菜单中显示
-        showLink: false,
-        activePath: "/tabs/index",
-        roles: ["admin", "common"]
-      }
-    },
-    // params 传参模式
-    {
-      path: "/tabs/params-detail/:id",
-      component: "params-detail",
-      name: "TabParamsDetail",
-      meta: {
-        // 不在menu菜单中显示
-        showLink: false,
-        activePath: "/tabs/index",
-        roles: ["admin", "common"]
-      }
-    }
-  ]
-};
-
 export default defineFakeRoute([
   {
     url: "/get-async-routes",
@@ -226,7 +43,7 @@ export default defineFakeRoute([
     response: () => {
       return {
         success: true,
-        data: [systemRouter, permissionRouter, frameRouter, tabsRouter]
+        data: [permissionRouter]
       };
     }
   }
